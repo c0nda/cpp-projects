f1af510badfb5469d1e64a0f74611e4e0c44ed61
Georgiy Voroncov <st095295@student.spbu.ru>
2021-10-25 16:37:13 +0300

    updated again

--- 25.10.2021/main.cpp
+++ 25.10.2021/main.cpp
@@ -52,21 +52,21 @@ void reverse_array() {
     std::cout << std::endl;
 
     for (int i = 0; i < n / 2; ++i) {
-            swap = array[i];
-            array[i] = array[n - 1 - i];
-            array[n - 1 - i] = swap;
+        swap = array[i];
+        array[i] = array[n - 1 - i];
+        array[n - 1 - i] = swap;
     }
     for (int i = 0; i <= n - 1; ++i) {
         std::cout << array[i] << " ";
     }
 }
 
-void moreLessAvg(){
+void moreLessAvg() {
     int swap;
     double avg, sum = 0;
     const int len = 10;
     int array[len];
-    for (int i = 0; i <= len - 1; ++i){
+    for (int i = 0; i <= len - 1; ++i) {
         array[i] = rand() % 100; // NOLINT(cert-msc30-c, cert-msc50-cpp)
         std::cout << array[i] << " ";
         sum += array[i];
@@ -75,10 +75,10 @@ void moreLessAvg(){
     std::cout << std::endl;
 
     avg = sum / double(len);
-    for (int i = 0; i <= len - 1; ++i){
-        if (array[i] > avg){
-            for (int j = len - 1; j > i; --j){
-                if (array[j] < avg){
+    for (int i = 0; i <= len - 1; ++i) {
+        if (array[i] > avg) {
+            for (int j = len - 1; j > i; --j) {
+                if (array[j] < avg) {
                     swap = array[i];
                     array[i] = array[j];
                     array[j] = swap;
@@ -87,16 +87,45 @@ void moreLessAvg(){
             }
         }
     }
-    for (int i = 0; i <= len - 1; ++i){
+    for (int i = 0; i <= len - 1; ++i) {
         std::cout << array[i] << " ";
     }
 
     std::cout << std::endl << avg;
 }
 
+void most_common_el() {
+
+}
+
+void couples() {
+    const int n = 5;
+    int array1[n] = {22, 21, 8, 6, 3};
+    int array2[n] = {0, 2, 4, 6, 8};
+    int answer[n * 2];
+    int j = 0;
+
+    for (int i = 0; i <= n - 1; ++i){
+        if (array1[i] >= array2[i]){
+            answer[i + j] = array2[i];
+            answer[i + 1 + j] = array1[i];
+            j += 1;
+        } else {
+            answer[i + j] = array1[i];
+            answer[i + 1 + j] = array2[i];
+            j += 1;
+        }
+    }
+    for (int i = 0; i <= n * 2 - 1; ++i) {
+        std::cout << answer[i] << " ";
+    }
+}
+
 int main() {
 //    minelemAndAverage();
 //    hex_array();
 //    reverse_array();
-    moreLessAvg();
+//    moreLessAvg();
+//    most_common_el();
+    couples();
 }
