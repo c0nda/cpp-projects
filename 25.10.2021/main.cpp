ef7a6efe25f74e527f0cd6814478dbb77d2c0fd7
Georgiy Voroncov <st095295@student.spbu.ru>
2021-10-25 12:28:10 +0300

    updated

--- 25.10.2021/main.cpp
+++ 25.10.2021/main.cpp
@@ -29,7 +29,19 @@ void minelemAndAverage() {
     std::cout << average;
 }
 
+void hex_array() {
+    int len;
+    std::cin >> len;
+    char rand_values[] = {};
+    char values[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    for (int i = 0; i < len; ++i){
+        rand_values[len] = values[0 + rand() % 15]; // NOLINT(cert-msc30-c, cert-msc50-cpp)
+        std::cout << rand_values[len] << " ";
+    }
+
+}
 
 int main() {
-    minelemAndAverage();
+//    minelemAndAverage();
+    hex_array();
 }
