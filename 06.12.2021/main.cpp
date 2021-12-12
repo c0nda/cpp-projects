d3dff8ef6ef4ff559dc705f44d2dc977e8c97c13
Georgiy Voroncov <st095295@student.spbu.ru>
2021-12-12 18:55:03 +0300

    added tasks with rgb

--- 06.12.2021/main.cpp
+++ 06.12.2021/main.cpp
@@ -33,33 +33,33 @@
 //    return 0;
 //}
 
-std::string bin(int c) {
-    int rem, counter = 0;
-    std::string answer;
-    char letter[] = {'0', '1'};
-    while (c > 0) {
-        rem = c % 2;
-        for (int i = 0; i <= 1; ++i) {
-            if (rem == i) {
-                answer += letter[i];
-                break;
-            }
-        }
-        counter += 1;
-        c /= 2;
-    }
-
-    for (int i = 0; i < counter / 2; ++i) {
-        std::swap(answer[i], answer[counter - 1 - i]);
-    }
-    unsigned int len = answer.length();
-    if (len < 8) {
-        for (int i = 0; i < 8 - len; ++i) {
-            answer = '0' + answer;
-        }
-    }
-    return answer;
-}
+//std::string bin(int c) {
+//    int rem, counter = 0;
+//    std::string answer;
+//    char letter[] = {'0', '1'};
+//    while (c > 0) {
+//        rem = c % 2;
+//        for (int i = 0; i <= 1; ++i) {
+//            if (rem == i) {
+//                answer += letter[i];
+//                break;
+//            }
+//        }
+//        counter += 1;
+//        c /= 2;
+//    }
+//
+//    for (int i = 0; i < counter / 2; ++i) {
+//        std::swap(answer[i], answer[counter - 1 - i]);
+//    }
+//    unsigned int len = answer.length();
+//    if (len < 8) {
+//        for (int i = 0; i < 8 - len; ++i) {
+//            answer = '0' + answer;
+//        }
+//    }
+//    return answer;
+//}
 
 // рисунок файла
 //int main(int argc, char *argv[]) {
@@ -96,30 +96,94 @@ std::string bin(int c) {
 //    return 0;
 //}
 
-int main(int argc, char *argv[]) {
-    if (argc > 2) {
-        std::ifstream ifs;
-        ifs.open(argv[1], std::ios_base::binary);
-        std::ofstream ofs;
-        ofs.open(argv[2], std::ios_base::binary);
-        if (ifs.is_open() && ofs.is_open()) {
-            ifs.seekg(10);
-            int px = 0;
-            ifs.read((char *) &px, sizeof(int));
-            ifs.seekg(px);
-            ofs.seekp(px);
-            int buffer[1];
-            for (int m = 0; m < 32; ++m) {
-                ifs.read((char *) &buffer, sizeof(int));
-                ofs.write((char *) &buffer, sizeof(int));
-                px += 16;
-                ofs.seekp(px);
-            }
-            ofs.close();
-            ifs.close();
-        } else {
-            std::cout << "Can't open file";
-        }
-    }
-    return 0;
-}
+// порядок хранения цветов rgb
+//int main(int argc, char *argv[]) {
+//    if (argc > 3) {
+//        std::ifstream red;
+//        red.open(argv[1], std::ios_base::binary);
+//        std::ifstream green;
+//        green.open(argv[2], std::ios_base::binary);
+//        std::ifstream blue;
+//        blue.open(argv[3], std::ios_base::binary);
+//        int px, buffer;
+//        if (red.is_open()) {
+//            red.seekg(10);
+//            red.read((char *) &px, sizeof(int));
+//            red.seekg(px + 3);
+//            std::cout << "Full red: ";
+//            for (int i = 0; i < 3; ++i) {
+//                red.read((char *) &buffer, sizeof(char));
+//                std::cout << buffer << " ";
+//            }
+//            red.close();
+//        } else {
+//            std::cout << "Can't open file" << std::endl;
+//        }
+//        std::cout << std::endl;
+//
+//        if (green.is_open()) {
+//            green.seekg(10);
+//            green.read((char *) &px, sizeof(int));
+//            green.seekg(px + 3);
+//            std::cout << "Full green: ";
+//            for (int i = 0; i < 3; ++i) {
+//                green.read((char *) &buffer, sizeof(char));
+//                std::cout << buffer << " ";
+//            }
+//            green.close();
+//        } else {
+//            std::cout << "Can't open file" << std::endl;
+//        }
+//        std::cout << std::endl;
+//
+//        if (blue.is_open()) {
+//            blue.seekg(10);
+//            blue.read((char *) &px, sizeof(int));
+//            blue.seekg(px + 3);
+//            std::cout << "Full blue: ";
+//            for (int i = 0; i < 3; ++i) {
+//                blue.read((char *) &buffer, sizeof(char));
+//                std::cout << buffer << " ";
+//            }
+//            blue.close();
+//        } else {
+//            std::cout << "Can't open file" << std::endl;
+//        }
+//    }
+//    return 0;
+//}
+
+// поменяли местами каналы красного и синего цвета(это не работает)
+//int main(int argc, char *argv[]) {
+//    if (argc > 2) {
+//        std::ifstream ifs;
+//        ifs.open(argv[1], std::ios_base::binary);
+//        std::ofstream ofs;
+//        ofs.open(argv[2], std::ios_base::binary);
+//        if (ifs.is_open() && ofs.is_open()) {
+//            int px, size;
+//            ifs.seekg(2);
+//            ifs.read((char *) &size, sizeof(int));
+//            ifs.seekg(10);
+//            ifs.read((char *) &px, sizeof(int));
+//            ifs.seekg(px);
+//            ofs.seekp(px);
+//            char buffer1[1];
+//            char buffer2[1];
+//            char buffer3[1];
+//            for (int i = 0; i < (size - px) / 3; ++i) {
+//                ifs.read((char *) &buffer1, sizeof(char));
+//                ifs.read((char *) &buffer2, sizeof(char));
+//                ifs.read((char *) &buffer3, sizeof(char));
+//                ofs.write(buffer3, sizeof(char));
+//                ofs.write(buffer2, sizeof(char));
+//                ofs.write(buffer1, sizeof(char));
+//            }
+//            ifs.close();
+//            ofs.close();
+//        } else {
+//            std::cout << "Can't open file" << std::endl;
+//        }
+//    }
+//    return 0;
+//}
