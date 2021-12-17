e8b7fe1dd3bbac2b2e74202a332e2fbb9e6e5d21
Georgiy Voroncov <st095295@student.spbu.ru>
2021-12-17 21:23:49 +0300

    new changes

--- 15.11.2021/main.cpp
+++ 15.11.2021/main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 #include "testArrays.h"
 
-//void search_area(int **arr, int len, int wid, int l_place, int w_place){
-//    if ((arr[l_place + 1][w_place] <= arr[l_place][w_place]) && (l_place + 1 < len)){
-//        ++arr[l_place + 1][w_place];
-//    }
-//    if ((arr[l_place - 1][w_place] <= arr[l_place][w_place]) && (l_place - 1 >= 0)){
-//        ++arr[l_place - 1][w_place];
-//    }
-//    if ((arr[l_place][w_place + 1] <= arr[l_place][w_place]) && (w_place + 1 < wid)){
-//        ++arr[l_place][w_place + 1];
-//    }
-//    if ((arr[l_place][w_place - 1] <= arr[l_place][w_place]) && (w_place - 1 >= 0)){
-//        ++arr[l_place][w_place - 1];
-//    }
-//    ++arr[l_place][w_place];
-//}
-//
-//void water(int **arr, int len, int wid, int steps, int l_place, int w_place){
-//    for (int i = 1; i <= steps; ++i){
-//        search_area(arr, len, wid, l_place, w_place);
-//    }
-//    printArray(arr, len, wid);
-//}
+void search_area(int **arr, int len, int wid, int l_place, int w_place, int **zeroes){
+    if ((arr[l_place + 1][w_place] <= arr[l_place][w_place]) && (l_place + 1 < len)){
+        zeroes[l_place + 1][w_place] = 1;
+        search_area(arr, len, wid, l_place + 1, w_place, zeroes);
+    }
+    if ((arr[l_place - 1][w_place] <= arr[l_place][w_place]) && (l_place - 1 >= 0)){
+        zeroes[l_place + 1][w_place] = 1;
+        search_area(arr, len, wid, l_place - 1, w_place, zeroes);
+    }
+    if ((arr[l_place][w_place + 1] <= arr[l_place][w_place]) && (w_place + 1 < wid)){
+        zeroes[l_place + 1][w_place] = 1;
+        search_area(arr, len, wid, l_place, w_place + 1, zeroes);
+    }
+    if ((arr[l_place][w_place - 1] <= arr[l_place][w_place]) && (w_place - 1 >= 0)){
+        zeroes[l_place + 1][w_place] = 1;
+        search_area(arr, len, wid, l_place, w_place - 1, zeroes);
+    }
+    ++zeroes[l_place][w_place];
+}
+
+void water(int **arr, int len, int wid, int steps, int l_place, int w_place){
+    int **zeroes = const_array(len, wid, 0);
+    for (int i = 1; i <= steps; ++i){
+        search_area(arr, len, wid, l_place, w_place, zeroes);
+    }
+    printArray(zeroes, len, wid);
+    delete_2D_indep_array(zeroes, len);
+}
 
 void testConst_array(int rows, int columns, int cnst) {
     int **arr = const_array(rows, columns, cnst);
@@ -38,7 +44,7 @@ void testConst_array(int rows, int columns, int cnst) {
     }
     std::cout << fail_count << " tests failed" << std::endl;
     std::cout << pass_count << " tests passed" << std::endl;
-    delete_2Darray(arr, rows);
+    delete_2D_indep_array(arr, rows);
     std::cout << std::endl;
 }
 
@@ -55,7 +61,7 @@ void test_ematrix(int rows, int columns) {
         }
         std::cout << fail_count << " tests failed" << std::endl;
         std::cout << pass_count << " tests passed" << std::endl;
-        delete_2Darray(arr, rows);
+        delete_2D_indep_array(arr, rows);
     } else {
         std::cout << "Test failed: input data is invalid" << std::endl;
     }
@@ -75,17 +81,35 @@ int main() {
 //
 //    water(ppi, 3, 3, 2, 1, 1);
 //    delete_2Darray(ppi, 3);
-    std::cout << "============= Tests const_array =============" << std::endl;
-    testConst_array(4, 3, 3);
-    testConst_array(3, 4, 1);
-    testConst_array(1, 1, 0);
-    testConst_array(1, 10, 12);
+//    std::cout << "============= Tests const_array =============" << std::endl;
+//    testConst_array(4, 3, 3);
+//    testConst_array(3, 4, 1);
+//    testConst_array(1, 1, 0);
+//    testConst_array(1, 10, 12);
+//
+//    std::cout << "============= Tests ematrix =============" << std::endl;
+//    test_ematrix(10, 10);
+//    test_ematrix(1, 1);
+//    test_ematrix(11, 1);
+//    test_ematrix(100, 100);
+//    test_ematrix(0, 1);
+//    int **arr = create_2D_indep_array<int>(3, 3);
+//    for (int i = 0; i < 3; ++i){
+//        for (int j = 0; j < 3; ++j){
+//            arr[i][j] = rand() % 10;
+//        }
+//    }
+//    printArray(arr, 3, 3);
+//    std::cout << std::endl << std::endl;
+//    water(arr, 3, 3, 3, 1, 1);
 
-    std::cout << "============= Tests ematrix =============" << std::endl;
-    test_ematrix(10, 10);
-    test_ematrix(1, 1);
-    test_ematrix(11, 1);
-    test_ematrix(100, 100);
-    test_ematrix(0, 1);
+    double **arr = random_array<double>(4, 5, 3);
+    printArray(arr, 4, 5);
+    std::cout << std::endl;
+    gauss_algorithm(arr, 4, 5);
+    printArray(arr, 4, 5);
+//    change2RowsOrColumns(arr, 4, 5, 'r', 0, 1, 1);
+//    printArray(arr, 4, 5);
+    delete_2D_indep_array(arr, 4);
     return 0;
 }
--- 15.11.2021/testArrays.h
+++ 15.11.2021/testArrays.h
@@ -1,10 +1,18 @@
 #ifndef INC_15_11_2021_TESTARRAYS_H
 #define INC_15_11_2021_TESTARRAYS_H
 
+double **random_double_array(int, int);
+
+void get_matrix_for_det(int **, int, size_t, size_t, int **);
+
+int determinant(int **, int);
+
+double **gauss_algorithm(double **, size_t, size_t);
+
 template<typename T>
-void printArray(T **arr, int rows, int columns) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
+void printArray(T **arr, size_t rows, size_t columns) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
             std::cout << arr[i][j] << "\t";
         }
         std::cout << std::endl;
@@ -13,40 +21,50 @@ void printArray(T **arr, int rows, int columns) {
 }
 
 template<typename T>
-void delete_2Darray(T **ppi, int rows) {
-    for (int i = 0; i < rows; ++i) {
+void delete_2D_indep_array(T **ppi, size_t rows) {
+    for (size_t i = 0; i < rows; ++i) {
         delete[] ppi[i];
     }
     delete[] ppi;
 }
 
 template<typename T>
-T **create_2D_indep_array(int rows, int columns) {
-    T **arr = new int *[rows];
-    for (int i = 0; i < rows; ++i) {
-        arr[i] = new int[columns];
+T **create_2D_indep_array(size_t rows, size_t columns) {
+    T **arr = new T *[rows];
+    for (size_t i = 0; i < rows; ++i) {
+        arr[i] = new T[columns];
     }
     return arr;
 }
 
 template<typename T>
-T **create_2D_dep_array(int rows, int columns) {
-    T *pi = new int[rows * columns]{0};
-    T **ppi = new int *[rows];
-    for (int i = 0, j = 0; i < rows * columns; i += columns, ++j) {
+T **create_2D_dep_array(size_t rows, size_t columns) {
+    T *pi = new T[rows * columns]{0};
+    T **ppi = new T *[rows];
+    for (size_t i = 0, j = 0; i < rows * columns; i += columns, ++j) {
         ppi[j] = &pi[i];
     }
     return ppi;
 }
 
+template<typename T>
+T** random_array(size_t rows, size_t columns, int num){
+    T **arr = create_2D_indep_array<T>(rows, columns);
+    for (size_t i = 0; i < rows; ++i){
+        for (size_t j = 0; j < columns; ++j){
+            arr[i][j] = rand() % num;
+        }
+    }
+    return arr;
+}
 
 template<typename T>
-T **const_array(int rows, int columns, T cnst) {
-    T **ppi = new int *[rows];
-    for (int i = 0; i < rows; ++i) {
-        ppi[i] = new int[columns];
+T **const_array(size_t rows, size_t columns, T cnst) {
+    T **ppi = new T *[rows];
+    for (size_t i = 0; i < rows; ++i) {
+        ppi[i] = new T[columns];
 
-        for (int j = 0; j < columns; ++j) {
+        for (size_t j = 0; j < columns; ++j) {
             ppi[i][j] = cnst;
         }
     }
@@ -54,11 +72,11 @@ T **const_array(int rows, int columns, T cnst) {
 }
 
 template<typename T>
-T **ematrix(int rows, int columns) {
+T **ematrix(size_t rows, size_t columns) {
     if (rows == columns) {
         T **ppi = create_2D_indep_array<T>(rows, columns);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < columns; ++j) {
                 if (i == j) {
                     ppi[i][j] = 1;
                 } else {
@@ -73,10 +91,10 @@ T **ematrix(int rows, int columns) {
 }
 
 template<typename T>
-T **t_matrix(T **arr, int rows, int columns) {
+T **t_matrix(T **arr, size_t rows, size_t columns) {
     T **trans = create_2D_indep_array<T>(columns, rows);
-    for (int i = 0; i < columns; ++i) {
-        for (int j = 0; j < rows; ++j) {
+    for (size_t i = 0; i < columns; ++i) {
+        for (size_t j = 0; j < rows; ++j) {
             trans[i][j] = arr[j][i];
         }
     }
@@ -84,9 +102,9 @@ T **t_matrix(T **arr, int rows, int columns) {
 }
 
 template<typename T>
-T **turn180(T **mirror, int rows, int columns) {
-    for (int i = 0; i < rows / 2; ++i) {
-        for (int j = 0; j < columns; ++j) {
+T **turn180(T **mirror, size_t rows, size_t columns) {
+    for (size_t i = 0; i < rows / 2; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
             std::swap(mirror[i][j], mirror[rows - 1 - i][columns - 1 - j]);
         }
     }
@@ -94,9 +112,9 @@ T **turn180(T **mirror, int rows, int columns) {
 }
 
 template<typename T>
-T **multiplyMatrixByNumber(T **matrix, int rows, int columns, int number) {
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
+T **multiplyMatrixByNumber(T **matrix, size_t rows, size_t columns, int number) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
             matrix[i][j] = matrix[i][j] * number;
         }
     }
@@ -104,10 +122,10 @@ T **multiplyMatrixByNumber(T **matrix, int rows, int columns, int number) {
 }
 
 template<typename T>
-T **create_multiplyMatrixByNumber(T **matrix, int rows, int columns, int number) {
+T **create_multiplyMatrixByNumber(T **matrix, size_t rows, size_t columns, int number) {
     T **result = create_2D_indep_array<T>(rows, columns);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
             result[i][j] = matrix[i][j] * number;
         }
     }
@@ -115,10 +133,10 @@ T **create_multiplyMatrixByNumber(T **matrix, int rows, int columns, int number)
 }
 
 template<typename T>
-T **additionOfMatrices(T **matrix1, T **matrix2, int rows, int columns) {
+T **additionOfMatrices(T **matrix1, T **matrix2, size_t rows, size_t columns) {
     T **result = create_2D_indep_array<T>(rows, columns);
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < columns; ++j) {
             result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
@@ -126,13 +144,13 @@ T **additionOfMatrices(T **matrix1, T **matrix2, int rows, int columns) {
 }
 
 template<typename T>
-T **multiplyMatrixByMatrix(T **matrix1, T **matrix2, int rows1, int columns1, int rows2, int columns2) {
+T **multiplyMatrixByMatrix(T **matrix1, T **matrix2, size_t rows1, size_t columns1, size_t rows2, size_t columns2) {
     T **result = create_2D_indep_array<T>(rows1, columns2);
     if (columns1 == rows2) {
-        for (int n = 0; n < rows1; ++n) {
-            for (int m = 0; m < columns2; ++m) {
+        for (size_t n = 0; n < rows1; ++n) {
+            for (size_t m = 0; m < columns2; ++m) {
                 result[n][m] = 0;
-                for (int k = 0; k < columns1; ++k) {
+                for (size_t k = 0; k < columns1; ++k) {
                     result[n][m] += matrix1[n][k] * matrix2[k][m];
                 }
             }
@@ -143,10 +161,10 @@ T **multiplyMatrixByMatrix(T **matrix1, T **matrix2, int rows1, int columns1, in
 }
 
 template<typename T>
-T **change2RowsOrColumns(T **arr, int rows, int columns, char name, int num1, int num2) {
+T **change2RowsOrColumns(T **arr, size_t rows, size_t columns, char name, size_t num1, size_t num2, size_t elem) {
     if (name == 'c') {
         if (num1 < columns & num2 < columns) {
-            for (int i = 0; i < rows; ++i) {
+            for (size_t i = elem; i < rows; ++i) {
                 std::swap(arr[i][num1], arr[i][num2]);
             }
         }
@@ -154,7 +172,7 @@ T **change2RowsOrColumns(T **arr, int rows, int columns, char name, int num1, in
 
     if (name == 'r') {
         if (num1 < rows & num2 < rows) {
-            for (int i = 0; i < columns; ++i) {
+            for (size_t i = elem; i < columns; ++i) {
                 std::swap(arr[num1][i], arr[num2][i]);
             }
         }
