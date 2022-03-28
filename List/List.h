e5dde06470f140b5ce2047139f558f8dc43b6dbd
Georgiy Voroncov <st095295@student.spbu.ru>
2022-03-28 11:58:26 +0300

    Create List.h

---
+++ List/List.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <cstring>
+
+template<typename T>
+class List {
+public:
+    List() {
+        size_ = 0;
+        first_ = nullptr;
+    }
+
+    List(const List<T> &lst) {
+        size_ = lst.size_;
+        first_ = new T;
+        std::memcpy(first_, lst.first_, 1);
+        while (lst.first_->next_ != nullptr) {
+            addElement(lst.first_->next_);
+            first_ = first_->next_;
+        }
+    }
+
+    List(List<T> &&) {}
+
+    void operator=(const List<T> &lst) {
+        if (this != &lst) {
+            delete first_;
+            size_ = lst.size_;
+            first_ = new T;
+            std::memcpy(first_, lst.first_, 1);
+            while (lst.first_->next_ != nullptr) {
+                addElement(lst.first_->next_);
+                first_ = first_->next_;
+            }
+        }
+        return *this;
+    };
+
+    ~List() {
+        if (first_ == nullptr) return;
+        do {
+            Node *cur = first_->next_;
+            delete first_;
+            first_ = cur;
+        } while (first_ != nullptr);
+    };
+
+    size_t getSize() const { return size_; }
+
+//    void removeElement(const Marker& );
+
+    void addElement(T newElem) {
+        Node *newnode = new Node(newElem);
+        if (first_ == nullptr) {
+            first_ = newnode;
+            ++size_;
+            std::cout << first_->data_;
+            return;
+        }
+        Node *cur = first_;
+        while (cur->next_ != nullptr) {
+            cur = cur->next_;
+        }
+        cur->next_ = newnode;
+        ++size_;
+    }
+
+    class ListMarker {
+    public:
+        ListMarker(){
+            cur_ = nullptr;
+        }
+
+        ListMarker(Node *p){
+            cur_ = nullptr;
+        }
+
+        T &getCurValue() {
+            return cur_->data_;
+        }
+
+        void moveNext() {
+            cur_ = cur_->next_;
+        }
+
+        bool canMoveNext() {
+            return (cur_->next_ != nullptr);
+        }
+
+    private:
+        Node *cur_;
+    };
+
+    ListMarker getMarker();
+
+private:
+    struct Node {
+        T data_;
+        Node *next_;
+
+        Node(const T &d) {
+            data_ = d;
+            next_ = nullptr;
+        }
+    };
+
+    Node *first_;
+    size_t size_;
+};
+
+
